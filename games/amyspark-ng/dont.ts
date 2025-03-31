0233a5525c13e2dc8f894e44c79e83abdd97ae84
amyspark-ng ✨ <92493175+amyspark-ng@users.noreply.github.com>
2025-03-31 00:11:40 -0500

    DONT revamp! :) (also removed flip)

--- games/amyspark-ng/dont.ts
+++ games/amyspark-ng/dont.ts
@@ -3,61 +3,106 @@ import { Minigame } from "../../src/game/types";
 const dontGame: Minigame = {
 	prompt: "don't",
 	author: "amyspark-ng",
-	rgb: (ctx) => ctx.mulfok.DARK_BLUE,
+	rgb: (ctx) => ctx.mulfok.VOID_VIOLET,
 	duration: 3,
 	urlPrefix: "games/amyspark-ng/assets/",
 	load(ctx) {
-		ctx.loadSprite("button", "sprites/dont/button.png", {
-			sliceX: 2,
-			sliceY: 1,
-			anims: {
-				"pressed": 0,
-				"dont": 1,
-			},
-		});
+		ctx.loadSprite("explosion", "sprites/dont/explosion.png", { sliceX: 7, sliceY: 1, anims: { "a": { from: 0, to: 6 } } });
+		ctx.loadSprite("button", "sprites/dont/button.png", { sliceX: 2, sliceY: 1 });
+		ctx.loadSprite("earth", "sprites/dont/earth.png");
+		ctx.loadSprite("frame", "sprites/dont/frame.png");
+		ctx.loadSprite("marky", "sprites/dont/marky.png");
+		ctx.loadSprite("moon", "sprites/dont/moon.png");
+		ctx.loadSprite("shooting", "sprites/dont/shooting.png");
+		ctx.loadSprite("stars", "sprites/dont/stars.png");
 		ctx.loadSound("explode", "sounds/explode.mp3");
 	},
 	start(ctx) {
-		let hasWon = true;
+		// positions itself
+		const frame = ctx.add([ctx.sprite("frame"), ctx.z(1)]);
+
+		const stars = ctx.add([ctx.sprite("stars"), ctx.pos()]);
+		const earth = ctx.add([ctx.sprite("earth"), ctx.pos(400, 480), ctx.anchor("center"), ctx.rotate(0)]);
+		const moon = ctx.add([ctx.sprite("moon"), ctx.pos(580, 290), ctx.anchor("center"), ctx.rotate(0)]);
 
 		const button = ctx.add([
-			ctx.sprite("button", { anim: "dont" }),
+			ctx.sprite("button"),
 			ctx.anchor("center"),
-			ctx.pos(ctx.center()),
+			ctx.pos(400, 436),
+			ctx.z(2),
 		]);
 
-		// oh no you didn't!!
-		function Did() {
-			hasWon = false;
+		function explodeEverything() {
 			ctx.lose();
-			ctx.wait(1.5, () => ctx.finish());
+			ctx.flashCam(ctx.WHITE, 0.1 / ctx.speed);
+			ctx.play("explode", { speed: 0.5 * ctx.speed });
 
-			button.play("pressed");
-			ctx.wait(0.1, () => {
-				button.play("dont");
+			const explosion = ctx.add([
+				ctx.sprite("explosion"),
+				ctx.anchor("bot"),
+				ctx.pos(406, 313),
+			]);
 
-				let explosions = 0;
-				let time = 0.1;
-				ctx.onUpdate(() => {
-					time -= ctx.dt();
-					if (time <= 0) {
-						if (explosions >= 35) return;
-						ctx.play("explode", { volume: 0.5 });
-						ctx.addKaboom(ctx.vec2(ctx.rand(0, ctx.width()), ctx.rand(0, ctx.height())), { scale: 1 + 0.5 * explosions });
-						explosions++;
-						time = 0.25 - 0.05 * explosions;
-					}
-				});
+			explosion.play("a", { speed: 10 * ctx.speed });
+			ctx.wait(0.5 / ctx.speed, () => {
+				ctx.finish();
 			});
 		}
 
 		ctx.onTimeout(() => {
-			if (!hasWon) return;
+			if (ctx.winState() != undefined) return;
 			ctx.win();
 			ctx.wait(0.5, () => ctx.finish());
 		});
 
-		ctx.onInputButtonPress("action", Did);
+		// adds either mark or shooting star
+		if (ctx.chance(0.25)) {
+			if (ctx.chance(0.5)) {
+				ctx.wait(1 / ctx.speed, () => {
+					const shooting = ctx.add([
+						ctx.sprite("shooting"),
+						ctx.opacity(),
+						ctx.anchor("center"),
+						ctx.pos(ctx.center().add(ctx.rand(50, 100), -100 + ctx.rand(-10, 10))),
+					]);
+
+					shooting.fadeOut(0.25 / ctx.speed).onEnd(() => {
+						shooting.destroy();
+					});
+
+					shooting.onUpdate(() => {
+						shooting.move(-100, 0);
+						if (ctx.winState() == false) shooting.destroy();
+					});
+				});
+			}
+			else {
+				const marky = ctx.add([
+					ctx.sprite("marky"),
+					ctx.pos(ctx.center().add(ctx.rand(-50, 50), ctx.rand(-100, -50))),
+					ctx.anchor("center"),
+					ctx.rotate(ctx.rand(90, 180)),
+				]);
+
+				let vel = ctx.vec2(-50, 0).scale(ctx.speed);
+				let angle = 0.5 * ctx.speed;
+				marky.onUpdate(() => {
+					if (ctx.winState() == false) {
+						vel = ctx.lerp(vel, ctx.vec2(-200, -350).scale(ctx.speed), 0.75);
+						angle = ctx.lerp(angle, 10 * ctx.speed, 0.75);
+					}
+					marky.move(vel);
+					marky.angle -= angle * ctx.speed;
+				});
+			}
+		}
+
+		ctx.onUpdate(() => {
+			button.frame = ctx.isInputButtonDown("action") ? 1 : 0;
+			stars.move(-ctx.rand(5, 10) * ctx.speed, 0);
+			earth.angle -= ctx.rand(0.005, 0.05) * ctx.speed;
+		});
+		ctx.onInputButtonPress("action", explodeEverything);
 	},
 };
 
